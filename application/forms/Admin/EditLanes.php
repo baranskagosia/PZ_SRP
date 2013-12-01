1ac625d148602225cb473f2113a4c563215a76a3
Mateusz Boczula <kareem.jabaar@gmail.com>
2013-12-01 19:00:35 +0100

    szkielet edycji liczby torów (widok + form. + akcja)

---
+++ application/forms/Admin/EditLanes.php
@@ -0,0 +1,29 @@
+<?php
+
+class Application_Form_Admin_EditLanes extends Zend_Form
+{
+    public function init()
+    {
+        
+        $optionsArray = array();
+        for($i=1; $i<=20; $i++)
+            $optionsArray["$i"] = "$i";
+        $this->setAction('rejestracja')->setMethod('post');
+        
+        $db = Zend_Db_Table::getDefaultAdapter();
+        $query_string = "SELECT COUNT(*) AS lanesCount FROM tor";
+        $result = $db->query($query_string)->fetch();
+        $lanesCount = $result['lanesCount'];
+        
+        $lanesNoSelector = new Zend_Form_Element_Select("LiczbaTorow");
+        $lanesNoSelector->setLabel("Liczba torów: ");
+        $lanesNoSelector->setMultiOptions($optionsArray);
+        $lanesNoSelector->setValue($lanesCount);
+        $this->addElement($lanesNoSelector);
+        
+        $this->addElement('submit', 'confirm', array('label' => 'Zatwierdź zmiany'));
+    }
+
+
+}
+
